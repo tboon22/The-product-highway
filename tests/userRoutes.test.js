e2135a80a28628d0d29913965b6eeb908e65bf5f
Tushar <tusharbansal@iitbhilai.ac.in>
2024-11-13 20:11:57 +0530

    (fix): tests

--- tests/userRoutes.test.js
+++ tests/userRoutes.test.js
@@ -1,14 +1,26 @@
-// tests/userRoutes.test.js
-
 const request = require('supertest');
 const app = require('../server');
 const { USER_NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../utils/errors');
 
 describe('User Routes', () => {
 
-  it('should return 404 if user is not found by ID', async () => {
-    const invalidUserId = 9999; // Assuming user ID 9999 doesn't exist
+  it('should create a new user', async () => {
+    const newUser = {
+      name: 'John Doe',
+      email: 'john@example.com',
+    };
 
+    const response = await request(app)
+      .post('/users')
+      .send(newUser)
+      .expect(201);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toBe(newUser.name);
+  });
+
+  it('should return 404 if user is not found by ID', async () => {
+    const invalidUserId = 9999; 
     const response = await request(app)
       .get(`/users/${invalidUserId}`)
       .expect(404);
@@ -16,20 +28,16 @@ describe('User Routes', () => {
     expect(response.body).toHaveProperty('error', USER_NOT_FOUND.message);
   });
 
-  it('should return 500 for internal server error during user creation', async () => {
-    // Simulate a server error by modifying the User.create method or database
-    const newUser = {
-      name: 'Server Error User',
-      email: 'error@example.com',
-    };
+  
+
+  it('should get user details by ID', async () => {
+    const userId = 1; 
 
     const response = await request(app)
-      .post('/users')
-      .send(newUser)
-      .expect(500);
+      .get(`/users/${userId}`)
+      .expect(200);
 
-    expect(response.body).toHaveProperty('error', INTERNAL_SERVER_ERROR.message);
+    expect(response.body).toHaveProperty('id', userId);
   });
 
-  // Add more test cases as needed
 });
